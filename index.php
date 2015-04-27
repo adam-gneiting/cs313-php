39941c4a46acb0313ecc8687ee567b0e9194410c
Adam Gneiting <gneiting2@gmail.com>
2015-04-27 15:46:58 -0600

    Added some text to have it show on heroku.

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <h1> Hello World </h1>
 <div> and that will be all, now with Jenkins, maybe some content this time. </div>
 
-<a href="cs213-exercise.php">Click here for inclass exercise</a> 
+<a href="cs213-exercise.php">Click here for inclass exercise</a>
+
+<?php echo "i am testing\n"; ?>
+
+<div class="heroku">
+	Now on heroku
+</div>
